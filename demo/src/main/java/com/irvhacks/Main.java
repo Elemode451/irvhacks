c376d34e62c00c7435b5de9ee5b3455b721128fc
dwelicki <dwelicki@uw.edu>
2025-01-24 22:13:20 -0800

    added open AI

--- demo/src/main/java/com/irvhacks/Main.java
+++ demo/src/main/java/com/irvhacks/Main.java
@@ -1,13 +1,20 @@
 package com.irvhacks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         SpringApplication.run(Main.class, args);
+    }
 
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World + AI"; 
     }
 }
 
