6d414ebd066998fbf78aba3d9bb28cb17fe60f93
dwelicki <dwelicki@uw.edu>
2025-01-25 16:03:17 -0800

    abandoning java

--- demo/src/main/java/com/irvhacks/Main.java
+++ demo/src/main/java/com/irvhacks/Main.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
 import io.github.cdimascio.dotenv.Dotenv;
+import io.micrometer.common.lang.NonNull;
 
 @SpringBootApplication
 @RestController
--- demo/src/main/java/com/irvhacks/QueryManager.java
+++ demo/src/main/java/com/irvhacks/QueryManager.java
@@ -1,8 +1,13 @@
 package com.irvhacks;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,49 +18,95 @@ import com.openai.models.ChatCompletionCreateParams;
 import com.openai.models.ChatCompletionUserMessageParam;
 import com.openai.models.ChatModel;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @RestController
 public class QueryManager {
-    
-    public static final String SEARCH_INFO = "You are to embody the role of a search engine." 
+    public static final String SEARCH_INFO = "You are to embody the role of a MEDICAL search engine." 
      + "You will only provide names in response to the" 
      + "query that is asked, with no other additional information." 
-     + "You will provide a list of up to 10 names. JUST names. Seperate each one with the character |."; 
+     + "You will provide a list of up to 15 names. JUST names. Seperate each one with the character |. Make sure it's a full name, and no weird artifacts are included."; 
     
-    public static final String DOCTOR_NAME = "If you are provided with a singular name, or asked for information on a name, just output the exact name back."; 
+    public static final String DOCTOR_NAME = "If you are provided with a singular name, or asked for information on a DOCTOR'S name, just output the exact name back."; 
+
+    private static HttpClient httpClient = HttpClient.newHttpClient();
     
+        @PostMapping("/query")
+        public Map<String, String> query(@RequestBody String query)
+        {
+            if (query == null || query.isEmpty()) {
+              Main.message = "NO!";
+            }
+            ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
+            .addMessage(ChatCompletionUserMessageParam.builder()
+              .role(JsonValue.from("system"))
+              .content(QueryManager.SEARCH_INFO)
+              .build())
+            .addMessage(ChatCompletionUserMessageParam.builder()
+              .role(JsonValue.from("system"))
+              .content(QueryManager.DOCTOR_NAME)
+              .build())
+            .addMessage(ChatCompletionUserMessageParam.builder()
+              .role(JsonValue.from("user"))
+              .content(query)
+              .build())
+            .model(ChatModel.GPT_4O_2024_11_20)
+            .temperature(0.4)
+            .build();
     
-    @PostMapping("/query")
-    public Map<String, String> query(@RequestBody String query)
-    {
-        if (query == null || query.isEmpty()) {
-          Main.message = "NO!";
+            ChatCompletion chatCompletion = Main.client.chat().completions().create(params).validate();  
+            String result = chatCompletion.choices().get(0).message().content().get();   
+            Main.message = result;
+    
+            return Map.of("result", result); 
         }
-        ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
-        .addMessage(ChatCompletionUserMessageParam.builder()
-          .role(JsonValue.from("system"))
-          .content(QueryManager.SEARCH_INFO)
-          .build())
-        .addMessage(ChatCompletionUserMessageParam.builder()
-          .role(JsonValue.from("system"))
-          .content(QueryManager.DOCTOR_NAME)
-          .build())
-        .addMessage(ChatCompletionUserMessageParam.builder()
-          .role(JsonValue.from("user"))
-          .content(query)
-          .build())
-        .model(ChatModel.GPT_4O_2024_11_20)
-        .temperature(0.7)
-        .build();
+    
+        private static class Zembra{ 
+            private static final String ZEMBRA_URI = "https://api.zembra.io/listing/find?name=";        
+    
+            public static List<HttpResponse<String>> queryZembra(List<String> doctors) {
+                List<HttpResponse<String>> responses = new ArrayList<>();
 
-        ChatCompletion chatCompletion = Main.client.chat().completions().create(params).validate();  
-        String result = chatCompletion.choices().get(0).message().content().get();   
-        Main.message = result;
+                try {
+                    List<HttpRequest> requests = new ArrayList<>();
+                    
+                    for(String doctor : doctors) {
+                        requests.add(HttpRequest.newBuilder()
+                                    .uri(URI.create(ZEMBRA_URI + doctor))
+                                    .GET()
+                                    .header("Accept", "application/json")
+                                    .header("Authorization", "Bearer " + Dotenv.load().get("ZEMBRA_API_KEY"))
+                                    .build());
+                    }
+        
 
-        System.out.println(result);
+                    for(var request : requests) {
+                        responses.add(httpClient.send(request, HttpResponse.BodyHandlers.ofString()));
+                    }
 
-        return Map.of("result", result); 
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+
+                return responses;
+        }
+
+        public static Map<String, Map<String, String>> getZembraResponse(List<HttpResponse<String>> responses) {
+            if(responses.isEmpty()) {
+                return null;
+            }
+
+            for(HttpResponse<String> response : responses) {
+
+            }
+        }
 
     }
+
+
+
+
+
     
 
 
